48488d6034a61749ee76ff1789fb9e90441fdeb7
Phil M. Strato <pmoessl@outlook.de>
2024-02-19 19:19:31 +0000

    add some packages

--- Funflix_backend/settings.py
+++ Funflix_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-vjmm^y#esi*i_#*-+a9m8#u32=-3*edmq^v3nhkc+%(vh5g39f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['194.164.206.243']
 
 CACHE_TTL = 60 * 15 #total lifetime 15min
 
